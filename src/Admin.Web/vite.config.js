433a98bf23c5538e1ceae5fc6c11f3a60daa3aef
Moises211 <moisesacosta8@gmail.com>
2026-06-01 14:58:32 -0600

    feat(config): load environment variables based on mode in Vite configuration

--- src/Admin.Web/vite.config.js
+++ src/Admin.Web/vite.config.js
@@ -1,22 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://gateway-service:3000', 
-        changeOrigin: true,
-        secure: false,
-      },
-      '/notificationHub': {
-        target: 'http://inventario-service:8080',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all envs regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      allowedHosts: env.VITE_ALLOWED_HOST ? [env.VITE_ALLOWED_HOST] : true,
+      proxy: {
+        '/api': {
+          target: 'http://gateway-service:3000', 
+          changeOrigin: true,
+          secure: false,
+        },
+        '/notificationHub': {
+          target: 'http://inventario-service:8080',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
+  }
 })
